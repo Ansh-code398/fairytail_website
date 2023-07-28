bd2e6695ae91d5779ca6fb243186bd6beb7cc379
Ansh Sharma Games and Technology <73456520+Ansh-code398@users.noreply.github.com>
2023-07-28 06:36:47 +0530

    update

--- pages/activities/[slug].js
+++ pages/activities/[slug].js
@@ -8,7 +8,7 @@ const Activity = ({activity, source}) => {
         <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
                 <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={activity.image} />
-                <div className="text-center lg:w-2/3 w-full prose">
+                <div className="text-center lg:w-2/3 w-full prose prose-xl">
                     <MDXRemote {...source} />
                 </div>
             </div>
